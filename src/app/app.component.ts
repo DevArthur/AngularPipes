06eaaa4e69d9f6d55f97664f93025c4ff28698d5
DevArthur <dev.lopez.alcaraz@gmail.com>
2023-04-11 00:52:05 -0600

    complted pipes section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '03-pipes';
+  
+  nombre: string = 'Capitán America';
+  nombre2 : string = 'ArtUro LópEZ AlcaRAz';
+  arreglo = [1,2,3,4,5,6,7,8,9];
+  PI = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha : Date = new Date();
+  activar : boolean = true;
+  idioma : string = 'es';
+  videoUrl : string = 'https://www.youtube.com/embed/ReaFJ_MD2cs';
+
+  cambiarIdioma(idioma : string){
+    this.idioma = idioma;
+  }
+  
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre:'Logan',
+    clave:'Wolverin',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
